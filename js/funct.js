821baa70f9979bd96d9aa553ba70799375326a48
Lucrecia Pedraza <63425878+lucre-ph@users.noreply.github.com>
2025-09-11 01:14:00 -0300

    updates

--- js/funct.js
+++ js/funct.js
@@ -1,5 +1,4 @@
-/* >Requerimiento obligatorio: Interactividad con JavaScript
-- La portada principal (index.html) debe incluir al menos una interacción dinámica implementada
+/*- La portada principal (index.html) debe incluir al menos una interacción dinámica implementada
 con JavaScript. Por ejemplo: mostrar un mensaje al hacer clic en un botón, animaciones simples,
 o cambios de estilo al interactuar con algún elemento de la página.
 - Cada página individual de un integrante debe contener al menos una función dinámica en
