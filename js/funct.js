f36986f806d52b3e25c5d86e6cb7c70d506f1d1c
Lucrecia Pedraza <63425878+lucre-ph@users.noreply.github.com>
2025-09-11 00:39:33 -0300

    js

--- js/funct.js
+++ js/funct.js
@@ -7,3 +7,20 @@ JavaScript, que modifique o actualice algún contenido de la tarjeta de presenta
 desplegar/ocultar información, cambiar colores, mostrar alertas, animaciones, etc.).
 - Todas las funciones implementadas deben estar mencionadas y descriptas en el archivo
 README.md, indicando qué hacen y en qué sección se encuentran. */
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("toggleInfo");
+  const infoSection = document.getElementById("infoSection");
+
+  toggleBtn.addEventListener("click", () => {
+    infoSection.classList.toggle("hidden");
+    infoSection.classList.toggle("showed");
+
+    if (infoSection.classList.contains("showed")) {
+      toggleBtn.textContent = "Ocultar Información"
+    } else {
+      toggleBtn.textContent = "Información Personal"
+    }
+  })
+});
