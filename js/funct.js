0aed3ca59d114e96470115735761a1b28d47cd06
Lucrecia Pedraza <63425878+lucre-ph@users.noreply.github.com>
2025-09-20 17:25:11 -0300

    updates

--- js/funct.js
+++ js/funct.js
@@ -1,13 +1,3 @@
-/*- La portada principal (index.html) debe incluir al menos una interacción dinámica implementada
-con JavaScript. Por ejemplo: mostrar un mensaje al hacer clic en un botón, animaciones simples,
-o cambios de estilo al interactuar con algún elemento de la página.
-- Cada página individual de un integrante debe contener al menos una función dinámica en
-JavaScript, que modifique o actualice algún contenido de la tarjeta de presentación (por ejemplo:
-desplegar/ocultar información, cambiar colores, mostrar alertas, animaciones, etc.).
-- Todas las funciones implementadas deben estar mencionadas y descriptas en el archivo
-README.md, indicando qué hacen y en qué sección se encuentran. */
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggleInfo");
   const infoSection = document.getElementById("infoSection");
--- js/scriptLu.js
+++ js/scriptLu.js
@@ -1,8 +1,7 @@
 // Declaracion de constantes
-
 const imagen = document.getElementById("pokedex");
 const imgOriginal = "../img/pokedex1.png";
-const imgNueva = "./img/pokedex2.png";
+const imgNueva = "../img/pokedex2.png";
 const btnMenu = document.getElementById("menu");
 const nav = document.querySelector(".linksNavegacion");
 
@@ -15,6 +14,4 @@ imagen.addEventListener("click", () => {
 // Mostrar y ocultar menu desplegable del navegador
 btnMenu.addEventListener("click", () => {
   nav.classList.toggle("active");
-
 });
-
