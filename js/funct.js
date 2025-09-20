b7d9c962f2cff80174544ed78a5f683dd846f064
Martín Comito <martin@cohley.com>
2025-09-20 19:54:32 -0300

    Prevención de errores en funct.js

--- js/funct.js
+++ js/funct.js
@@ -3,58 +3,52 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("botonIndex");
   const colores = ["#f4a261", "#2a9d8f", "#e9c46a", "#264653", "#e76f51"];
 
+  if (!btn) return;
   btn.addEventListener("click", () => {
     const randomColor = colores[Math.floor(Math.random() * colores.length)];
     document.body.style.backgroundColor = randomColor;
   });
 });
 
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("toggleInfo");
   const infoSection = document.getElementById("infoSection");
 
+  if (!toggleBtn || !infoSection) return;
   toggleBtn.addEventListener("click", () => {
     infoSection.classList.toggle("hidden");
     infoSection.classList.toggle("showed");
 
     if (infoSection.classList.contains("showed")) {
-      toggleBtn.textContent = "Ocultar Información"
+      toggleBtn.textContent = "Ocultar Información";
     } else {
-      toggleBtn.textContent = "Información Personal"
+      toggleBtn.textContent = "Información Personal";
     }
-  })
-
+  });
 });
-
 
 document.addEventListener("DOMContentLoaded", () => {
-    const avatar = document.querySelector(".avatar");
-    const aliasParrafo = document.querySelector(".tarjeta p strong"); 
+  const avatar = document.querySelector(".avatar");
+  const aliasParrafo = document.querySelector(".tarjeta p strong");
 
-    let esGris = true;
+  let esGris = true;
 
-    avatar.addEventListener("click", () => {
-        avatar.classList.add("fade"); 
+  if (!avatar || !aliasParrafo) return;
 
-        setTimeout(() => {
-            if (esGris) {
-                avatar.src = "./img/gandalf2.png"; 
-                aliasParrafo.textContent = "Alias: Lichalf el Blanco";
-            } else {
-                avatar.src = "./img/gandalf1.png"; 
-                aliasParrafo.textContent = "Alias: Lichalf el Gris";
-            }
-            esGris = !esGris;
+  avatar.addEventListener("click", () => {
+    avatar.classList.add("fade");
 
-            avatar.classList.remove("fade"); 
-        }, 600);
-    });
+    setTimeout(() => {
+      if (esGris) {
+        avatar.src = "./img/gandalf2.png";
+        aliasParrafo.textContent = "Alias: Lichalf el Blanco";
+      } else {
+        avatar.src = "./img/gandalf1.png";
+        aliasParrafo.textContent = "Alias: Lichalf el Gris";
+      }
+      esGris = !esGris;
+
+      avatar.classList.remove("fade");
+    }, 600);
+  });
 });
-
-
-
