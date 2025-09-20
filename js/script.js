7c1df429aaff82ef293f02c983678f4876ada16b
Lucrecia Pedraza <63425878+lucre-ph@users.noreply.github.com>
2025-09-20 15:56:15 -0300

    upload scriptLu

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Declaracion de constantes
+
+const imagen = document.getElementById("pokedex");
+const imgOriginal = "/pokedex1.png";
+const imgNueva = "/pokedex2.png";
+const btnMenu = document.getElementById("menu");
+const nav = document.querySelector(".linksNavegacion");
+
+// Funciones y eventos
+// Cambiar imagen al hacer click en pokedex
+imagen.addEventListener("click", () => {
+  imagen.src = imagen.src.includes("pokedex1") ? imgNueva : imgOriginal;
+});
+
+// Mostrar y ocultar menu desplegable del navegador
+btnMenu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
