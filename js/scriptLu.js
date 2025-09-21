3edefb7fc31471faa8f5cf65d35f839d20ec2424
Lucrecia Pedraza <63425878+lucre-ph@users.noreply.github.com>
2025-09-21 15:46:37 -0300

    Update scriptLu.js

--- js/scriptLu.js
+++ js/scriptLu.js
@@ -4,16 +4,23 @@ const imgOriginal = "./img/pokedex1.png";
 const imgNueva = "./img/pokedex2.png";
 const btnMenu = document.getElementById("menu");
 const nav = document.querySelector(".linksNavegacion");
+const saludo = document.querySelector(".bienvenida");
 
 // Funciones y eventos
-// Cambiar imagen al hacer click en pokedex
+// Cambiar imagen al hacer click en pokedex y ocultar saludo
 imagen.addEventListener("click", () => {
   imagen.src = imagen.src.includes("pokedex1") ? imgNueva : imgOriginal;
+  
+  //No incluye zoom en pantallas chicas
+  if (window.innerWidth >= 800) {
+    imagen.src.includes("pokedex2") ? imagen.classList.add("zoom") : imagen.classList.remove("zoom");
+  }
+
+  saludo.style.display = saludo.style.display === "none" ? "block" : "none";
+  
 });
 
 // Mostrar y ocultar menu desplegable del navegador
 btnMenu.addEventListener("click", () => {
   nav.classList.toggle("active");
-
 });
-
