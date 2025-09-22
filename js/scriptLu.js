52277daa0ded8450f10427476e7db96baaaf9fb6
Lucrecia Pedraza <63425878+lucre-ph@users.noreply.github.com>
2025-09-22 08:32:21 -0300

    Update scriptLu.js

--- js/scriptLu.js
+++ js/scriptLu.js
@@ -5,6 +5,15 @@ const imgNueva = "./img/pokedex2.png";
 const btnMenu = document.getElementById("menu");
 const nav = document.querySelector(".linksNavegacion");
 const saludo = document.querySelector(".bienvenida");
+const playlist = [
+  "../music/Pewter City.mp3", 
+  "../music/Cerulean City.mp3", 
+  "../music/Celadon City.mp3", 
+  "../music/Cinnabar Island.mp3", 
+  "../music/Pallet Town.mp3", 
+  "../music/Vermillion City.mp3", 
+  "../music/Pewter City.mp3", 
+  "../music/Surfing.mp3"];
 
 // Funciones y eventos
 // Cambiar imagen al hacer click en pokedex y ocultar saludo
@@ -23,4 +32,15 @@ imagen.addEventListener("click", () => {
 // Mostrar y ocultar menu desplegable del navegador
 btnMenu.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+// Reproducir musica y cambiar canciones
+let index = 0;
+const player = document.querySelector(".controlAudio");
+
+player.src = playlist[index];
+player.addEventListener("ended", () => {
+  index = (index + 1) % playlist.length;
+  player.src = playlist[index];
+  player.play();
 });
